0e8e95587aaf7051ec75d1048c3f466f65b130ed
in-up <inup@kyonggi.ac.kr>
2023-05-13 21:34:12 +0900

    deleted:    app/favicon.ico 	modified:   app/globals.css 	modified:   app/layout.js 	modified:   app/page.js 	modified:   app/page.module.css 	new file:   public/favicon.png 	new file:   public/images/favicon.ico

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,31 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Youlu Downloader',
-  description: '율루로 다운로드',
-}
+  title: 'Youlu 단축어 추가하기',
+  description: 'iPhone에서 율루로 다운로드하세요.',
+};
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet" />
       <div className='main'>
         <div className='nav'>
-          <div className='btn'><a href="/"><span className="material-icons-round" style={{ fontSize: '25px'}}>home</span></a></div>
-          <div className='btn'><a href="/"><span className="material-icons-round" style={{ fontSize: '25px'}}>login</span></a></div>
+          <div className='btn'><a href="/youlu"><span className="material-icons-round" style={{ fontSize: '25px',color:'rgb(72, 128, 238)'}}>home</span></a></div>
+          <div className='btn'><a href="/youlu"><span className="material-icons-round" style={{ fontSize: '25px',color:'rgb(72, 128, 238)'}}>login</span></a></div>
         </div>
-
       </div>
       {children}
     </html>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 export default function Home() {
   return (
@@ -14,8 +14,8 @@ export default function Home() {
       <div className={styles.desc}>
       <h4 className='noto_sans'>Youlu 단축어 하나로<br/>유튜브, 트위터, 릴스, 커뮤니티 사이트의 영상을 다운로드할 수 있어요.<br/></h4>
       </div>
-
-    <div className={styles.download}><h3 className={styles.download_btn}>단축어 추가 (Beta)</h3></div>
+    <a href="https://www.icloud.com/shortcuts/"><div className={styles.download}><h3 className={styles.download_btn}>단축어 추가 (Beta)</h3></div>
+</a>
     </main>
   )
 }

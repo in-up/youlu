c0705f24f48ae05b23fd734f8d92783e838f643b
in-up <inup@kyonggi.ac.kr>
2023-05-13 22:54:40 +0900

    modified:   app/api/page.js 	new file:   app/api/page.module.css 	modified:   app/layout.js 	modified:   app/page.js

--- app/api/page.js
+++ app/api/page.js
@@ -1,10 +1,22 @@
+'use client'
+
 import Image from 'next/image'
-import classNames from 'classnames';
+import styles from './page.module.css'
+import React from 'react'
+
+const handleCopyClipBoard = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    alert('클립보드에 링크가 복사되었습니다.');
+  } catch (e) {
+    alert('복사에 실패하였습니다');
+  }
+};
 
 export default function Home() {
   return (
-    <body>
-        <p>1.0.0</p>
-    </body>
+    <main>
+        <p>version:"1.0.0"</p>
+    </main>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
 import classNames from 'classnames';
+
 
 export default function Home() {
   return (
@@ -9,7 +11,7 @@ export default function Home() {
         <img className={styles.preview}src="/b1.png"></img>
       </div>
       <div className={styles.title}>
-      <h1 className='noto_sans_X'>Youlu 다운로더</h1>
+      <h1 className='noto_sans_X'>Youlu 다운로드</h1>
       </div>
       <div className={styles.desc}>
       <h4 className='noto_sans'>Youlu 단축어 하나로<br/>유튜브, 트위터, 릴스, 커뮤니티 사이트의 영상을 <br/>다운로드할 수 있어요.<br/></h4>
